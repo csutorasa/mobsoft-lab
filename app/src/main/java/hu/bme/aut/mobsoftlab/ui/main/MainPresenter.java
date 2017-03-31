1bd31944aba69c208d42649f5688715987413167
Ármin Csutorás <csutorasa@gmail.com>
2017-03-31 09:49:23 +0200

    MVP implementation

---
+++ app/src/main/java/hu/bme/aut/mobsoftlab/ui/main/MainPresenter.java
@@ -0,0 +1,28 @@
+package hu.bme.aut.mobsoftlab.ui.main;
+
+import hu.bme.aut.mobsoftlab.ui.Presenter;
+
+public class MainPresenter extends Presenter<MainScreen> {
+
+    private static MainPresenter instance = null;
+
+    private MainPresenter() {
+    }
+
+    public static MainPresenter getInstance() {
+        if (instance == null) {
+            instance = new MainPresenter();
+        }
+        return instance;
+    }
+
+    @Override
+    public void attachScreen(MainScreen screen) {
+        super.attachScreen(screen);
+    }
+
+    @Override
+    public void detachScreen() {
+        super.detachScreen();
+    }
+}
---
+++ app/src/main/java/hu/bme/aut/mobsoftlab/ui/main/MainScreen.java
@@ -0,0 +1,5 @@
+package hu.bme.aut.mobsoftlab.ui.main;
+
+public interface MainScreen {
+    void showMessage(String text);
+}
